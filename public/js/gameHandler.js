79beb144f6aeb03451ab0fa6ba60f004df1bca87
Dewiter <herolezero@gmail.com>
2021-06-15 13:37:37 +0200

    fixed import issue with classes

--- public/js/gameHandler.js
+++ public/js/gameHandler.js
@@ -1,40 +1,9 @@
-class Game {
-  constructor(block) {
-    const sketch = this.createGrid(block);
-    new p5(sketch, 'container');
-  }
-    createGrid(block) {
-        const grid = [];
-        for (let i = 0; i < 8; i++) {
-          grid[i] =  new Array();
-          let r = Math.floor (block * Math.random());
-          for (let j = 0; j < 8; j++) {
-            if (r == j) {
-              grid[i][j] = "F";  
-            } else  {
-              grid[i][j] = "E";
-            }
-          }
-          block++;
-        }
-        console.log(grid);
-        let sketch = function(p) {
-            p.setup = function(){
-              p.createCanvas(400, 400);
-              p.background(255);
-            }
-            p.draw = function () {
-              let y = 0;
-              for (let y = 0; y < 400; y+= 50) {
-                for (let x = 0; x < 400; x+= 50) {
-                  p.rect(x, y, 50, 50);
-                };
-                
-              }
-            }
-          };
-        return (sketch);
-    }
+"use strict"
+import  Map  from './map.js'
 
-    
+class Game {
+  constructor(size, block) {
+    const map = new Map();
+  }   
 }
+export default Game
--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,23 @@
- export let sketch = function(p) {
-    p.setup = function(){
-      p.createCanvas(400, 400);
-      p.background(255);
+"use strict"
+
+class Map {
+  constructor (size, block) {
+    let sketch;
+    sketch = this.initCancas()
+    new p5(sketch, 'container');
+
+  }
+
+  initCancas() {
+    const sketch = function (p) {
+      p.setup = () => {
+        p.createCanvas(400, 400);
+        p.background(255);
+      }
     }
-  };
+    return sketch;
+  }
+
+}
+
+export default Map
