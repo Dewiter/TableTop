4077d0099f1cde1c61077232514763ed7d25da68
Dewiter <herolezero@gmail.com>
2021-06-15 15:19:54 +0200

    json implemented

--- public/js/gameHandler.js
+++ public/js/gameHandler.js
@@ -1,9 +1,10 @@
 "use strict"
 import  Map  from './map.js'
 
+
 class Game {
-  constructor(size, block) {
-    const map = new Map();
-  }   
+  constructor(config) {
+    const map = new Map(config.map);
+  } 
 }
 export default Game
--- public/js/map.js
+++ public/js/map.js
@@ -1,21 +1,46 @@
 "use strict"
 
 class Map {
-  constructor (size, block) {
-    let sketch;
-    sketch = this.initCancas()
+  // init class
+  constructor (config) {
+    const grid    = this.createGrid(config.size, config.block);
+    const sketch  = this.drawCanvas(grid);
     new p5(sketch, 'container');
 
   }
 
-  initCancas() {
+  //@desc     Creates none walkable tile
+  //@block    defines the number of tiles not walkable
+  createBlock(block) {
+
+  }
+
+  // @desc    Creates a grid array
+  // @size    array that defines size of the map
+  // @block   defines the number of tiles not walkable
+  createGrid(size, block) {
+    const grid = []
+    for (let x = 0; x < size[0]; x++) {
+      grid[x] = [];
+      for (let y = 0; y < size[1]; y++) {
+        grid[x][y] = "E";
+      }
+    }
+    console.log(grid);
+    return grid;
+  }
+
+
+  // @desc    Draws and creates a p5 canvas
+  // @grid    Array that defines the map
+  drawCanvas(grid) {
     const sketch = function (p) {
       p.setup = () => {
         p.createCanvas(400, 400);
         p.background(255);
-      }
+      } 
     }
-    return sketch;
+    return sketch
   }
 
 }
